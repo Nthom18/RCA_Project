a4272651130c03d4f17a40018742c179f1c93eb1
nthom18 <nthom18@student.sdu.dk>
2020-11-16 06:48:09 -0800

    Draw chuncky circles on map

--- Custom_Robot_Controller/scr/trackPos.cpp
+++ Custom_Robot_Controller/scr/trackPos.cpp
@@ -3,6 +3,26 @@
 
 void track(cv::Mat map, int x, int y)
 {
+    static const cv::Point gz_x(-5, 5); // These values are found by measuring it in the Gazebo env.
+    static const cv::Point gz_y(4, -4);
+
+    static const cv::Point map_x(0, map.cols);
+    static const cv::Point map_y(0, map.rows);
+
+    // std::cout << "GZ:  " << x << " " << y << std::endl;
+
+    x = intervalMapping(x, gz_x, map_x);
+    y = intervalMapping(y, gz_y, map_y);
+
+    // std::cout << "MAP: " << x << " " << y << std::endl;
+
     cv::Point mark(x, y);
     cv::circle(map, mark, 3, cv::Vec3b(255, 0, 0), -1);
+}
+
+int intervalMapping(int s, cv::Point from, cv::Point to)
+{
+    // Formula for mapping number from one range to another
+    int t = to.x + ((s - from.x)*(to.y - to.x))/(from.y - from.x);
+    return t;
 }
--- Custom_Robot_Controller/scr/trackPos.hpp
+++ Custom_Robot_Controller/scr/trackPos.hpp
@@ -1,3 +1,5 @@
 #include <opencv2/opencv.hpp>
 
 void track(cv::Mat map, int x, int y);
+
+int intervalMapping(int s, cv::Point from, cv::Point to);
